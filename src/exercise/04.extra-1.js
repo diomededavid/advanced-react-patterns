d1208bd4531acae76c4b55ff9319529ab785dd74
David Diomede <david@d2creativedesign.com>
2022-07-10 19:06:38 -0700

    Prop Collections and Getters

---
+++ src/exercise/04.extra-1.js
@@ -0,0 +1,58 @@
+// Prop Collections and Getters
+// http://localhost:3000/isolated/exercise/04.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+function callAll(...fns) {
+  return (...args) => {
+    fns.forEach(fn => {
+      fn && fn(...args)
+    })
+  }
+}
+
+function useToggle() {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  function getTogglerProps({onClick, ...props} = {}) {
+    return {
+      'aria-pressed': on,
+      onClick: callAll(onClick, toggle),
+      ...props,
+    }
+  }
+  return {
+    on,
+    toggle,
+    getTogglerProps,
+  }
+}
+
+function App() {
+  const {on, getTogglerProps} = useToggle()
+  return (
+    <div>
+      <Switch {...getTogglerProps()} />
+      <hr />
+      <button
+        aria-label="custom-button"
+        {...togglerProps}
+        onClick={() => {
+          console.info('onButtonClick')
+          togglerProps.onClick()
+        }}
+      >
+        {on ? 'on' : 'off'}
+      </button>
+    </div>
+  )
+}
+
+export default App
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
